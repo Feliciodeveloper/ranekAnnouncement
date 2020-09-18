58ddf4a1d80940d260fe39ce77700f5ff9ecdfc0
Felicio Neto <felicio.developer@gmail.com>
2020-09-17 22:54:12 -0300

    Componentes header e footer criado

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <p>Ranek, alguns direitos reservados</p>
+    <p>Desenvolvido por FN</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+};
+</script>
+
+<style scoped>
+footer {
+  background: #87f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1rem;
+  color: #fff;
+  position: relative;
+  top: 90px;
+}
+</style>
